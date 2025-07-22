21e4b06cf71a88ce2d470d5d23df04038d864ab1
Shahad Al Harthy <160311849+Li1-sha@users.noreply.github.com>
2025-07-22 16:37:33 +0400

    Add UserInfo class with basic properties.java

--- UserInformationSystem/src/UserInfo.java
+++ UserInformationSystem/src/UserInfo.java
@@ -10,39 +10,30 @@ public class UserInfo {
         this.email = email;
         this.isActive = isActive;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public int getAge() {
         return age;
     }
-
     public void setAge(int age) {
         this.age = age;
     }
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
     public boolean isActive() {
         return isActive;
     }
-
     public void setActive(boolean active) {
         isActive = active;
     }
-
     public void displayInfo(){
         System.out.println("\nName: "+getName());
         System.out.println("Age: "+getAge());
