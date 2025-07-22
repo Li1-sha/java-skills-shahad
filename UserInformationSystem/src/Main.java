52e983eb1a51fcbc1242888094a65973b97071b8
Shahad Al Harthy <160311849+Li1-sha@users.noreply.github.com>
2025-07-22 16:14:52 +0400

    Task 2.1 UserInformation System

---
+++ UserInformationSystem/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+    UserInfo user1 = new UserInfo("Shahad",20,"shahadthy15@gmail.com", true);
+    UserInfo user2 = new UserInfo("Ali",21,"ali453@icloud.com",false);
+    UserInfo user3 = new UserInfo("Raghad",18,"rbeast24@gmail.com",true);
+
+    user1.displayInfo();
+    user2.displayInfo();
+    user3.displayInfo();
+
+    user1.setEmail("s.harthy@icloud.com");
+    System.out.println(user1.getEmail());
+    user2.setName("Omar");
+    System.out.println(user2.getName());
+    user3.setAge(19);
+    System.out.println(user3.getAge());
+
+    }
+}
---
+++ UserInformationSystem/src/UserInfo.java
@@ -0,0 +1,52 @@
+public class UserInfo {
+    private String name;
+    private int age;
+    private String email;
+    private boolean isActive;
+
+    public UserInfo(String name, int age, String email, boolean isActive) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.isActive = isActive;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public void displayInfo(){
+        System.out.println("\nName: "+getName());
+        System.out.println("Age: "+getAge());
+        System.out.println("Email: "+getEmail());
+        System.out.println("Is Active? "+isActive());
+    }
+}
